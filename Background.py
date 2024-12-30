5227334fd5f0d52a2383a44f92b35c7de68d0d02
John Hayduk <jhayduk@cisco.com>
2024-12-30 15:35:11 -0500

    Refactor Background to use "import module" instead of "from module import function" for readability when the functions are used. Also, change code that checks if pygame is initialized to raise an exception instead of locally doing the init.

--- Background.py
+++ Background.py
@@ -1,11 +1,9 @@
 import pygame
-from pygame import Surface
-from pygame.transform import scale, smoothscale
-from typing import override
+import typing
 
 from arcade_tools.GameElement import GameElement
 
-_BACKGROUND_IMAGE_FILE="./images/space.png"
+_BACKGROUND_IMAGE_FILE = "./images/space.png"
 
 
 class Background(GameElement):
@@ -13,7 +11,7 @@ class Background(GameElement):
     Background is a GameElement that is expected to be drawn first for each
     frame so that its image is behind every other game element.
     """
-    def __init__(self, screen: Surface):
+    def __init__(self, screen: pygame.Surface):
         """
         When instantiated, the __init__method is passed the screen surface so
         that this class can make sure that the image loaded will cover the
@@ -23,15 +21,12 @@ class Background(GameElement):
                         drawn over it. This is passed in during initialization
                         so that the size of the screen can be determined.
         """
-        #
-        # Make sure pygame is initialized. Normally, this is expected to be
-        # done before elements are created, so issue a warning if it had to
-        # be done here.
-        #
+        # Make sure pygame is initialized.
         if not pygame.get_init():
-            print(
-                f"WARNING: pygame was not initialized when a {self.__class__.__name__} object was instantiated. It has now been initialized, but pygame.init() should normally be called before instantiating any instances of the {self.__class__.__name__} class.")
-            pygame.init()
+            raise RuntimeError(
+                f"Pygame must be initialized before creating a {self.__class__.__name__} object. "
+                f"Please call pygame.init() before using this class."
+            )
 
         #
         # Initialize the base GameElement class
@@ -43,25 +38,26 @@ class Background(GameElement):
         #
         # Scale the background image to match the size of the screen.
         try:
-            self.image = smoothscale(self.image, screen.get_size())
+            self.image = pygame.transform.smoothscale(self.image, screen.get_size())
         except ValueError:
             """
-            smoothscale can throw a ValueError for a number of reasons including
-            if the original surface is not a 24-bit or 32-bit surface. On the off
-            chance this is the issue, try using scale() instead of smoothscale()
+            pygame.transform.smoothscale can throw a ValueError for a number
+            of reasons including if the original surface is not a 24-bit or
+            32-bit surface. On the off-chance this is the issue, try using
+            pygame.transform.scale() instead of pygame.transform.smoothscale()
             if the latter just failed.
             """
-            self.image = scale(self.image, screen.get_size())
+            self.image = pygame.transform.scale(self.image, screen.get_size())
 
-    @override
-    def update(self, *args, **kargs):
+    @typing.override
+    def update(self, *args, **kwargs):
         """
         The Background stays once displayed, so the GameElement update method
         is bypassed.
         """
         pass
 
-    @override
+    @typing.override
     def collided_with(self, other_element: GameElement):
         """
         The Background does not participate in collision detection, so this
