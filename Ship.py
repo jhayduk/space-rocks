fb70df0bb1801cebc5f897652939a3ec643c2bc4
John Hayduk <jhayduk@cisco.com>
2025-01-01 13:13:52 -0500

    Rename the parameters passed into __init__ and add an orientation vector for each ship instance.

--- Ship.py
+++ Ship.py
@@ -2,12 +2,17 @@ import pygame
 
 from arcade_tools.GameElement import GameElement
 
+#
+# The image of the ship is assumed to be positioned such that the front of the
+# ship is on the top of the picture. In other words, the image is assumed to
+# be of a ship with an orientation vector of (0, -1) (i.e. ↑)
+#
 _SHIP_IMAGE_FILE = "./images/ship.png"
 
 
 class Ship(GameElement):
     """
-    The Ship is a subclass of GameElement, which means it is updatable,
+    Ship is a subclass of GameElement, which means it is updatable,
     participates in collisions, and is drawable on the game screen.
 
     Because it is a GameElement, it has a rect attribute which has x and y
@@ -19,20 +24,25 @@ class Ship(GameElement):
     top left corner of the game screen itself, which is defined to be at
     x=0, y=0.
 
-    The Ship also has a velocity which is a Vector2 value in pixels per
+    A Ship also has a velocity which is a Vector2 value in pixels per
     millisecond (ppm), with a unit vector pointing 1 ppm to the right and
     1 ppm down (↘).
+
+    A Ship also has an orientation which changes as the ship rotates. The
+    orientation is a unit vector that points in the direction that the front
+    of the ship is facing. The thrusters always increase the velocity in the
+    direction of the orientation vector.
 
     Initially, there is only ever one ship in the game. However, this class
     is written to allow for a multiplayer option in the future.
     """
-    def __init__(self, center_x, center_y):
+    def __init__(self, ship_center_x: int, ship_center_y: int):
         """
 
-        :param center_x: The x component of the position where the center of
-                            the ship should be when first instantiated,
-        :param center_y: The y component of the position where the center of
-                            the ship should be when first instantiated,
+        :param ship_center_x: The x component of the position where the center
+                                of the ship should be when first instantiated,
+        :param ship_center_y: The y component of the position where the center
+                                of the ship should be when first instantiated,
         """
         # Make sure pygame is initialized.
         if not pygame.get_init():
@@ -45,7 +55,10 @@ class Ship(GameElement):
         super().__init__(_SHIP_IMAGE_FILE)
 
         # Reposition the ship so that the center of if it is at the desired position
-        self.rect.center = (center_x, center_y)
+        self.rect.center = (ship_center_x, ship_center_y)
+
+        # Set the starting orientation of the ship to be straight up
+        self.orientation = pygame.math.Vector2(0, -1)
 
     #
     # GameElement's update(), collide_with() and draw() methods are, currently,
